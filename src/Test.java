fde9adc642c74d9f58203e7b8ec82a01fc8877d9
wabster341 <jakub.dmitruk1999@gmail.com>
2024-10-21 14:48:17 +0200

    zadanie 1

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("sprawdzam czy dziala");
+        System.out.println("sprawdzam czy nadal dziala");
     }
+    
 }
